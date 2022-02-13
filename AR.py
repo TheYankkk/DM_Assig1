dbc954ba774fd1c2f1792b66bc2ac290e45ba8a6
Bob Liu <527080292@qq.com>
2022-02-13 21:39:37 +0800

    establish

--- AR.py
+++
@@ -1,3 +0,0 @@
-from mlxtend.preprocessing import TransactionEncoder
-import pandas as pd
-#print("test")
---
+++ Assig1/AR/Exercise/AR.py
@@ -0,0 +1,6 @@
+from mlxtend.preprocessing import TransactionEncoder
+import pandas as pd
+#print("test")
+import pandas as pd
+data=pd.read_csv(r"mammographic_masses.csv",delimiter=" ",header=None)
+print(data)
