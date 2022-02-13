2f29089a82e4a93764c26c0ee9e49543d7146118
Bob Liu <527080292@qq.com>
2022-02-14 04:29:14 +0800

    a little try

--- Assig1/AR/Exercise/AR.py
+++ Assig1/AR/Exercise/AR.py
@@ -1,6 +1,25 @@
-from mlxtend.preprocessing import TransactionEncoder
-import pandas as pd
 #print("test")
 import pandas as pd
-data=pd.read_csv(r"mammographic_masses.csv",delimiter=" ",header=None)
-print(data)
+import math
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+data=pd.read_csv(r"mammographic_masses.csv",delimiter=",",header=0)
+#print(data)
+d=data.values.tolist()
+for each in range(len(d)):
+    d[each][-1]=str(d[each][-1])
+#print(d)
+te = TransactionEncoder()
+te_ary = te.fit(d).transform(d)
+#print(te.columns_)
+df = pd.DataFrame(te_ary,columns=te.columns_)
+#print(df)
+
+#computing frequent itemsets and association rules
+frequent_itemsets = apriori(df, min_support=0.2, use_colnames=True)
+
+a=association_rules(frequent_itemsets, metric="confidence", min_threshold=0.9)
+
+#visualizing association rules results
+print(a[["antecedents","consequents","support","confidence"]])
