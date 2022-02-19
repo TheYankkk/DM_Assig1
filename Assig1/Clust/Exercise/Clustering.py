fe324ccbc69239c734b6f8f85853dd9e3a63d034
Bob Liu <527080292@qq.com>
2022-02-20 01:33:29 +0800

    test

--- Assig1/Clust/Exercise/Clustering.py
+++ Assig1/Clust/Exercise/Clustering.py
@@ -1,9 +1,17 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import matplotlib.pyplot as plt
 
 #create a numpy array
-X = np.array([[0, 0], [0, 1/2],[1, 1/2], [1, 1],[4, 0], [4, 1], [5, 1]])
-orignal= np.array([[0, 0], [1, 1]])
-kmeans = KMeans(init=orignal, n_clusters=2, max_iter=10000, n_init=100).fit(X)
+X = np.array([[0, 0], [0, 1/2],[1, 1/2], [1, 1],[3, 0], [3, 1], [4, 1]])
+x=np.array([0,0,1,1,3,3,4])
+y=np.array([0,1/2,1/2,1,0,1,1])
 
+orignal= np.array([[0, 0], [1, 1],[2.5,0.8]])
+kmeans = KMeans(init=orignal, n_clusters=3, max_iter=10000, n_init=100).fit_predict(X)
+center = KMeans(init=orignal, n_clusters=3, max_iter=10000, n_init=100).fit(X)
+plt.scatter(x,y,c=kmeans)
+for center in center.cluster_centers_:
+    plt.scatter(center[0], center[1], marker="p", edgecolors="red")
+plt.show()
 print(kmeans.cluster_centers_)
