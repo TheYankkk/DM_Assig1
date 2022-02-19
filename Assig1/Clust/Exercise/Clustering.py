ff663134fed5539d05d15e1d0a1fa374ecb01267
Bob Liu <527080292@qq.com>
2022-02-20 04:21:57 +0800

    question 1

--- Assig1/Clust/Exercise/Clustering.py
+++ Assig1/Clust/Exercise/Clustering.py
@@ -2,12 +2,15 @@ from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from sklearn import preprocessing
+#question 1
 data=pd.read_csv(r"clustering_data.csv",delimiter=",",header=0,index_col=0)
+#scaler=preprocessing.StandardScaler().fit(data)
 d=data.values.tolist()
 X=np.array(d)
 kmeans = KMeans(init="random").fit(X)
 print(kmeans.labels_)
-s=kmeans.labels_.shape
-print(s)
+SSE=kmeans.inertia_
+print(SSE)
 
+#question 2
